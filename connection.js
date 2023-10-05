f8cb61d15692a2ab1f53284e45351edbbc194503
manavmalavia18 <114517641+manavmalavia18@users.noreply.github.com>
2023-10-05 16:48:18 -0400

    eigth  commit

--- connection.js
+++ connection.js
@@ -1,6 +1,7 @@
 const Sequelize = require('sequelize')
 require('dotenv').config()
-const mariadb = require("mariadb");
+// const mariadb = require("mariadb");
+const mysql=require('mysql2/promise')
 
 const sequelize = new Sequelize(process.env.DB_NAME,process.env.DB_USER, process.env.DB_PASSWORD, {
     host: process.env.DB_HOST,
@@ -8,17 +9,23 @@ const sequelize = new Sequelize(process.env.DB_NAME,process.env.DB_USER, process
     dialect: process.env.DIALECT,
 });
 
-const pool =mariadb.createPool({
+// const pool =mariadb.createPool({
+//     host: process.env.DB_HOST,
+//     user: process.env.DB_USER,
+//     password: process.env.DB_PASSWORD
+// })
+async function dbconnect() {
+    
+    //    await pool.getConnection().then((connection) =>{
+    //     connection.query(`CREATE DATABASE IF NOT EXISTS\`${process.env.DB_NAME}\`;`)
+    //   });
+    await mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD
-})
-async function dbconnect() {
-    
-       await pool.getConnection().then((connection) =>{
-        connection.query(`CREATE DATABASE IF NOT EXISTS\`${process.env.DB_NAME}\`;`)
-      });
-    
+  }).then((connection) => {
+    connection.query(`CREATE DATABASE IF NOT EXISTS\`${process.env.DATABASE_NAME}\`;`)
+  })
    
     const User = require('./models/user')
 

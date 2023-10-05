de6985b54d6dc2c7b2a91d32a262238abf420485
manavmalavia18 <114517641+manavmalavia18@users.noreply.github.com>
2023-10-05 18:28:55 -0400

    11 the commit

--- test/healthz_test.js
+++ test/healthz_test.js
@@ -30,11 +30,45 @@
 //   });
 // });
 
+// const server = require("../connection.js").dbconnect;
+// const { describe } = require("mocha");
+// const chai = require("chai");
+// const chaiHttp = require("chai-http");
+// const app = require("../index.js").app; // Adjust the path to your app's entry point
+
+// const { expect } = chai;
+
+// chai.use(chaiHttp);
+
+// describe("CI Testing for GET/healthz", () => {
+//   it("Successfully check the Db connection", async () => {
+//     let dbstatus = true;
+//     server()
+//     try {
+//       const response = await chai.request(app).get("/healthz");
+//       expect(response).to.have.status(200);
+//     } catch (error) {
+//       console.error("Test Error:", error);
+
+//       dbstatus = false;
+
+//       expect(dbstatus, "Database connection failed").to.be.true;
+//     } 
+//     finally {
+//       setTimeout(() => {
+//         process.exit(0);
+//       }, 2000); //
+//     }
+//   });
+//   // setTimeout(() => {
+//   //   process.exit(0);
+//   // }, 5000); //
+// });
 const server = require("../connection.js").dbconnect;
-const { describe } = require("mocha");
+const { describe, it } = require("mocha");
 const chai = require("chai");
 const chaiHttp = require("chai-http");
-const app = require("../index.js").app; // Adjust the path to your app's entry point
+const app = require("../index.js").app;
 
 const { expect } = chai;
 
@@ -43,24 +77,21 @@ chai.use(chaiHttp);
 describe("CI Testing for GET/healthz", () => {
   it("Successfully check the Db connection", async () => {
     let dbstatus = true;
-    server()
-    try {
-      const response = await chai.request(app).get("/healthz");
-      expect(response).to.have.status(200);
-    } catch (error) {
-      console.error("Test Error:", error);
 
-      dbstatus = false;
+    // Wrap the test logic in a Promise
+    return new Promise(async (resolve, reject) => {
+      server();
 
-      expect(dbstatus, "Database connection failed").to.be.true;
-    } 
-    finally {
-      setTimeout(() => {
-        process.exit(0);
-      }, 2000); //
-    }
+      try {
+        const response = await chai.request(app).get("/healthz");
+        expect(response).to.have.status(200);
+        resolve(); // Resolve the Promise when the test is complete
+      } catch (error) {
+        console.error("Test Error:", error);
+        dbstatus = false;
+        expect(dbstatus, "Database connection failed").to.be.true;
+        reject(error); // Reject the Promise if the test fails
+      }
+    });
   });
-  // setTimeout(() => {
-  //   process.exit(0);
-  // }, 5000); //
 });

238b762ae789806da514a7967fab0c4acefbbbab
manavmalavia18 <114517641+manavmalavia18@users.noreply.github.com>
2023-10-24 13:50:28 -0400

    ass6-6

--- setup.sh
+++ setup.sh
@@ -1,10 +1,1 @@
-#!/bin/bash
 
-# Reload systemd to apply changes to service unit files
-sudo systemctl daemon-reload
-
-# Start the service
-sudo systemctl start systemd_packer
-
-# Enable the service to start on boot
-sudo systemctl enable systemd_packer
